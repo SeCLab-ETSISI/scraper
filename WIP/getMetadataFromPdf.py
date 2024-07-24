e0a8869e4525d355c53f200ef40e1c293c740e40
w-dan <d.galgora@upm.es>
2024-07-24 12:30:50 +0200

    [METADATA][FIX] pdf refactor

--- WIP/getMetadataFromPdf.py
+++ WIP/getMetadataFromPdf.py
@@ -1,61 +1,51 @@
-#!pip install pypdf
-#!pip install pdftitle
-
 from pypdf import PdfReader
-from pdftitle import  get_title_from_file
+from pdftitle import get_title_from_file
 
-
-def getMetadata(file1):
-
-      """
-    Gets metadata from a given pdf file
- 
-    :param file1: path + filename of the pdf file from which metadata is to be extracted
-
-    returns a dict with all metadata extracted
+def get_metadata(file_path):
     """
-  
-  reader = PdfReader(file1)
-  meta = reader.metadata
- 
-  print("Number of pages : ",len(reader.pages))
+    Gets metadata from a given PDF file.
 
-  # All of the following could be None!
-  
-  dict = {"Title":meta.title,"Author":meta.author,"Subject":meta.subject,"Creator":meta.creator,"Producer":meta.producer,"Creation Date":meta.creation_date,"Modification Date":meta.modification_date}
-  dict["XMP Metadata"] = meta.xmp_metadata
-
-  return dict
-
-def getTitleFromPdf(filePath1):
-
-      """
-    Gets Title from a given pdf file
- 
-    :param filePath1: path + filename of the pdf file from which metadata is to be extracted
-
-    returns a the title obtained
+    :param file_path: Path + filename of the PDF file from which metadata is to be extracted.
+    :return: A dictionary with all metadata extracted.
     """
-  
-  if(getTitleFromPdfAux(filePath1) == ""):
-    # returns the title specified in the metadata
-    return PdfReader(filePath1).metadata.title
-  else:
-    # returns the title obtained
-    return getTitleFromPdfAux(filePath1)
+    reader = PdfReader(file_path)
+    meta = reader.metadata
 
+    print("Number of pages:", len(reader.pages))
 
-def getTitleFromPdfAux(filePath1):
+    metadata_dict = {
+        "Title": meta.title,
+        "Author": meta.author,
+        "Subject": meta.subject,
+        "Creator": meta.creator,
+        "Producer": meta.producer,
+        "Creation Date": meta.creation_date,
+        "Modification Date": meta.modification_date,
+        "XMP Metadata": meta.xmp_metadata
+    }
 
-      """
-    Auxiliar method for getTitleFromPdf
- 
-    :param filePath1: path + filename of the pdf file from which metadata is to be extracted
+    return metadata_dict
 
-    returns the title obtained
+def get_title_from_pdf(file_path):
     """
-  
-  title = get_title_from_file(filePath1)
-  print("Title:", title)
-  return title
+    Gets the title from a given PDF file.
 
+    :param file_path: Path + filename of the PDF file from which the title is to be extracted.
+    :return: The title obtained.
+    """
+    title = get_title_from_pdf_aux(file_path)
+    if not title:
+        return PdfReader(file_path).metadata.title
+    else:
+        return title
+
+def get_title_from_pdf_aux(file_path):
+    """
+    Auxiliary method for get_title_from_pdf.
+
+    :param file_path: Path + filename of the PDF file from which the title is to be extracted.
+    :return: The title obtained.
+    """
+    title = get_title_from_file(file_path)
+    print("Title:", title)
+    return title
