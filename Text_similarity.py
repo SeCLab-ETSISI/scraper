36903647cf1273f2618921a29f099fd43b1a98a2
acdelrio <78924111+acdelrio@users.noreply.github.com>
2024-08-02 12:35:25 +0200

    Update Text_similarity.py
    [Text_similarity][FIX] added descriptions

--- Text_similarity.py
+++ Text_similarity.py
@@ -9,6 +9,15 @@ from datasketch import MinHash
 #import nltk
 
 def getMinHashFromFullText(texto):
+
+   """
+    Returns the minhash of a given text.
+
+    Parameters:
+    text: text from which to get the minhash
+    returns mn_a: minhash from text a
+    """
+  
   #set_a = [word for word in texto if word not in stopwords.words('english')]
   #Quizas remover las stopwords usando nltk stopwords
   set_a = texto.split()
@@ -18,6 +27,16 @@ def getMinHashFromFullText(texto):
   return mh_a
 
 def getSimilarityFromMinHashes(mh_a,mh_b):
+
+     """
+    Returns similarity between two given hashes.
+
+    Parameters:
+    mh_a: minhash from first text
+    mh_b: minhash from second text
+    returns similarity: number between 0 and 1, closer to 1 means more similar 
+    """
+  
   similarity = mh_a.jaccard(mh_b)
   return similarity
 
